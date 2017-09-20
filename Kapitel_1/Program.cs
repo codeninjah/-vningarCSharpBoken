7956ae0d37ed28b6ef37aa290c032020832f2eb4
Alexandry Cornita <alexandry.cornita.spo16@ecusolna.onmicrosoft.com>
2017-09-20 08:56:41 +0200

    Inga ändringar förutom punkt 2

---
+++ Kapitel_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapitel_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			//övnuppg 2
+			Console.WriteLine("Alexandru");
+
+			//övnuppg 3
+
+
+		}
+	}
+}
